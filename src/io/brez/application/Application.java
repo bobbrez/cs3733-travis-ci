141dc6b15447372bee885114ba59c32545059b05
Bob Breznak <bob.breznak@gmail.com>
2014-03-25 01:42:37 -0400

    Add name getter

--- src/io/brez/application/Application.java
+++ src/io/brez/application/Application.java
@@ -1,6 +1,10 @@
 package io.brez.application;
 
 public class Application {
+	public String name() {
+		return "My Application";
+	}
+	
 	public boolean isWorking() {
 		return isDemoMode() || !isBroken();
 	}
